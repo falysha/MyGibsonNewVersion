1fdb70c2f16395def25dddd0d9c45a778f038d09
GenSter <2744338955@qq.com>
2022-10-16 14:59:54 +0800

    阅读系统与白膜

---
+++ Assets/Scripts/InteractionSystem/Dialog/DialogTrigger.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    [Header("弹出UI")]
+    [SerializeField] public GameObject visualCue;//互动图标
+
+    private bool playerInRange;//玩家是否处于对话中
+
+    [Header("墨水文件")]
+    [SerializeField] public TextAsset inkJson;//json文件
+
+
+    private void Awake()
+    {
+        playerInRange = false;
+        visualCue.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(playerInRange && !DialogManager.GetInstance().DialogueIsPlaying)
+        {
+            visualCue.SetActive(true);
+            if(Input.GetButtonDown("interaction"))//互动键
+            {
+                DialogManager.GetInstance().EnterDialogueMode(inkJson);
+            }
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+}
