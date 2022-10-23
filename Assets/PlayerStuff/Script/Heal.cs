4456e1871470380d466cf5810ad78cd38bb3459f
Zaishi <87322835+PunkerFu@users.noreply.github.com>
2022-10-23 19:17:15 +0800

    10.23

---
+++ Assets/PlayerStuff/Script/Heal.cs
@@ -0,0 +1,22 @@
+﻿using Platformer.Core;
+using Platformer.Mechanics;
+using UnityEngine;
+
+namespace Platformer.Gameplay
+{
+    /// <summary>
+    /// Fired when the player performs a Jump.
+    /// </summary>
+    /// <typeparam name="PlayerJumped"></typeparam>
+    public class PlayerHeal : Simulation.Event<PlayerHeal>
+    {
+        private PlayerHealth _playerHealth=GameObject.Find("Player").GetComponent<PlayerHealth>();
+        private SkillController _skillController = GameObject.Find("Player").GetComponent<SkillController>();
+        
+        public override void Execute()
+        {
+            _playerHealth.realHealth = 100;
+            _skillController.Fury = 100;
+        }
+    }
+}
